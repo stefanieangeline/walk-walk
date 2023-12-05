e206bc1db67daf583a123972866bc2395fe5fb0e
NicMatthew <113076301+NicMatthew@users.noreply.github.com>
2023-12-05 09:23:23 +0700

    login

--- walk-walk/resources/views/login.blade.php
+++ walk-walk/resources/views/login.blade.php
@@ -10,12 +10,18 @@
 <body>
     <div class="container">
        <div class="container_picture_logo">
-        <img class="picture" src="https://drive.google.com/uc?export=view&id=1jPbHmzgPwXno9lg2lngldOWChWTrjcww" alt="">
+        <img class="picture" src="assets\images\login.png" alt="">
+        <div class="logo_container">
+            <img src="assets\logo\jalan-jalan.svg" alt="">
+
+        </div>
+        <p class="slogan">Explore the world your way: <br> your journey start with us</p>
 
        </div>
        <div class="form_sign">
         <div class="form_sign_container">
-            <p class="create_account">Log In</p>
+            <p class="create_account">Welcome back!</p>
+            <p class="create_account opacity">Log in to your account</p>
             <div class="divider-login">
 
             </div>
@@ -31,7 +37,7 @@
     
         <div class="sign-in">
             <b class="log_in_button">Don't have an account? </b>
-            <a href="{{route('sign-in')}}" class="log_in_button_click">Click here to sign Up</a>
+            <a href="{{route('sign-in')}}" class="log_in_button_click">Sign Up</a>
 
         </div>
         
