bc33dbc4abc152530dd2e70ca53ed66b20c9dd02
jesslyntanu <129813528+jesslyntanu@users.noreply.github.com>
2024-01-04 10:09:42 +0700

    review cust page done

--- walk-walk/resources/views/hotel-cust-review.blade.php
+++ walk-walk/resources/views/hotel-cust-review.blade.php
@@ -9,7 +9,39 @@
 <body>
     @include("shared.nav-bar-standard")
     <div class="background">
-        <img src="" alt="">
+    </div>
+    <div class="content">
+        <img src="\assets\images\hotel1.jpg" class="hotel-image">
+        <div class="box-content">
+            <div class="hotel-desc">
+                <div class="title-hotel">Arunika Hotel & Spa</div>
+                <div class="review-loc-hotel">
+                    <div class="review-hotel">100 reviews</div>
+                    <div class="bullet"></div>
+                    <div class="loc-hotel">Bali, Indonesia</div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="hotel-review">
+                <div class="review-question">How was your stay at Arunika Hotel and Spa?</div>
+                <div class="rating-with-star">
+                    <div class="rate-text">Rate your stay</div>
+                    <div class="star-box">
+                        <img src="\assets\icon\star-gold-line.png" class="star">
+                        <img src="\assets\icon\star-gold-line.png" class="star">
+                        <img src="\assets\icon\star-gold-line.png" class="star">
+                        <img src="\assets\icon\star-gold-line.png" class="star">
+                        <img src="\assets\icon\star-gold-line.png" class="star">
+                    </div>
+                </div>
+            </div>
+            <div class="review-box">
+                <div class="review-text">Write your review</div>
+            </div>
+        </div>
+        <div class="submit-box">
+            <div class="submit-text">SUBMIT</div>
+        </div>
     </div>
 </body>
 </html>
