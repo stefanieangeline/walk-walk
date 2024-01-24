2b4fed6c6279fbb1612d9b40d10354d180ed5350
jesslyntanu <129813528+jesslyntanu@users.noreply.github.com>
2024-01-24 13:49:09 +0700

    airport detail updateee

--- walk-walk/resources/views/airport-detail.blade.php
+++ walk-walk/resources/views/airport-detail.blade.php
@@ -8,13 +8,17 @@
     <link rel="stylesheet" href="\css\font-and-color.css">
     <link rel="stylesheet" href="\css\airport-detail.css">
     <script src="https://kit.fontawesome.com/4d9121ebec.js" crossorigin="anonymous"></script>
+    <script src="/js/airport-detail.js" defer=""></script>
 </head>
 
 <body>
+    <!-- dd($IDTicket);   -->
     <nav class="navbar">
         <div class="navbar-container">
             <div class="navbar-left">
-                <i class="fa-solid fa-angle-left"></i>
+                <!-- <a href="{{route("paymentSuccessful")}}"> -->
+                    <i class="fa-solid fa-angle-left"></i>
+                <!-- </a> -->
             </div>
             <div class="navbar-middle">
                 <p>Airport Details</p>
@@ -36,12 +40,14 @@
                 <!-- <div class="airport-name">
                     <p>Haneda Airport</p>
                 </div> -->
-                <div class="dropdown-airport">
-                    <div class="select">
-                        <div>Leonardo Da Vinci International Airport</div>
-                        <!-- <option value="Option 2"></option> -->
-                    </div>
-                </div>
+        <!-- dd({{$AirportSource->IDAirportSource}}) -->
+                <form name="dropdown-airport" method="GET">
+                    <select name="IDAirport" id="drop1">
+                        <option value="{{$AirportSource->IDAirportSource}}" @if ($IDAirport == $AirportSource->IDAirportSource) selected="selected" @endif> {{$AirportSource -> NameAirport}}</option>
+                        <option value="{{$AirportDestination->IDAirportDestination}}" @if ($IDAirport == $AirportDestination->IDAirportDestination) selected="selected" @endif> {{$AirportDestination -> NameAirport}}</option>
+                    </select>
+                    <input type="text" value="{{$IDTicket}}" class="hidden" name="IDTicket">
+                    
             </div>
             <div class="sort-category">
                 <div class="icon-category">
@@ -49,101 +55,35 @@
                 </div>
                 <div class="dropdown-category">
                     <div class="select-tenant">
-                        <form action="GET">
-                        <select id="standard-select">
+                        <select id="drop2">
                             <option value="All">All</option>
                             <option value="Retail">Shops <i class="fa-solid fa-shop"></i></option>
                             <option value="Food & Beverage">Restaurants <i class="fa-solid fa-utensils"></i></option>
                             <option value="Electronics">Electronics <i class="fa-solid fa-money-bill"></i></option>
                             <option value="Health & Wellness">Health & Wellness <i class="fa-solid fa-pills"></i></option>
                         </select>
+                        
                         </form>
                 </div>
                 </div>
             </div>
         </div>
+        
         <div class="body-content">
             <div class="all-tenant">
-                <div class="tenant tenant-1">
+                @foreach($TenantsAirport as $tenants)
+                <div class="tenants">
                     <div class="tenant-logo">
-                        <img src="/assets/logo/lotte-duty-free-logo.png" alt="logo1">
+                        <img src="\assets\images\tenants_logo\{{$tenants->IDTenants}}.png" alt="logo1" class="logo-img">
                     </div>
                     <hr class="line">
                     <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
+                        <p class="tenant-name">{{$tenants->NameTenants}}</p>
+                        <p class="tenant-category">{{$tenants->CategoryTenants}}</p>
                     </div>
                 </div>
-                <div class="tenant tenant-2">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-3">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-4">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-5">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-6">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-7">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
-                <div class="tenant tenant-8">
-                    <div class="tenant-logo">
-                        <img src="/jalan-jalan/assets/logo/lotte-duty-free-logo.png" alt="logo1">
-                    </div>
-                    <hr class="line">
-                    <div class="tenant-detail">
-                        <p class="tenant-name">Lotte Duty Free</p>
-                        <p class="tenant-category">Shop</p>
-                    </div>
-                </div>
+                @endforeach
+               
             </div>
         </div>
     </div>
