a492d2a5b5308da0dede074709a20fffe4e6fead
Joxnhby <114238028+Joxnhby@users.noreply.github.com>
2024-02-02 09:16:49 +0700

    documentation

--- walk-walk/public/js/airport-detail.js
+++ walk-walk/public/js/airport-detail.js
@@ -1,15 +1,13 @@
 drop1 = document.getElementById("drop1")
 drop2 = document.getElementById("drop2")
 
-console.log(drop1, drop2)
-
-
+// trigger when drop down got changed value
 function addClickEvent(el) {
     el.addEventListener("change", (e)=>{
         document.forms["dropdown-airport"].submit()
-        // document.forms["IDTicket"].submit()
     })
 }
 
+// set event listener
 addClickEvent(drop2)
 addClickEvent(drop1)
